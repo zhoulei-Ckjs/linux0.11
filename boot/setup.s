b294f2bdbe854e587ac89799142d60b75968743c
zhoulei-Ckjs <56968749@qq.com>
2026-01-17 11:17:14 +0800

    20260117

--- boot/setup.s
+++ boot/setup.s
@@ -33,150 +33,150 @@ start:
 ; ok, the read went well so we get current cursor position and save it for
 ; posterity.
 
-	mov	ax,#INITSEG	; this is done in bootsect already, but...
-	mov	ds,ax
-	mov	ah,#0x03	; read cursor pos
-	xor	bh,bh
-	int	0x10		; save it in known place, con_init fetches
-	mov	[0],dx		; it from 0x90000.
+    mov	ax,#INITSEG	; this is done in bootsect already, but...
+    mov	ds,ax
+    mov	ah,#0x03	; read cursor pos
+    xor	bh,bh
+    int	0x10		; save it in known place, con_init fetches
+    mov	[0],dx		; it from 0x90000.
 
 ; Get memory size (extended mem, kB)
 
-	mov	ah,#0x88	; 获取扩展内存，1M以上
-	int	0x15
-	mov	[2],ax		; 扩展内存总大小（KB），放到 ds:2内存上
+    mov	ah,#0x88	; 获取扩展内存，1M以上
+    int	0x15
+    mov	[2],ax		; 扩展内存总大小（KB），放到 ds:2内存上
 
 ; Get video-card data:
 
-	mov	ah,#0x0f
-	int	0x10
-	mov	[4],bx		; bh = display page
-	mov	[6],ax		; al = video mode, ah = window width
+    mov	ah,#0x0f
+    int	0x10
+    mov	[4],bx		; bh = display page
+    mov	[6],ax		; al = video mode, ah = window width
 
 ; check for EGA/VGA and some config parameters
 
-	mov	ah,#0x12
-	mov	bl,#0x10
-	int	0x10
-	mov	[8],ax
-	mov	[10],bx
-	mov	[12],cx
+    mov	ah,#0x12
+    mov	bl,#0x10
+    int	0x10
+    mov	[8],ax
+    mov	[10],bx
+    mov	[12],cx
 
 ; Get hd0 data
 
-	mov	ax,#0x0000
-	mov	ds,ax
-	lds	si,[4*0x41]
-	mov	ax,#INITSEG
-	mov	es,ax
-	mov	di,#0x0080
-	mov	cx,#0x10
-	rep
-	movsb
+    mov	ax,#0x0000
+    mov	ds,ax
+    lds	si,[4*0x41]
+    mov	ax,#INITSEG
+    mov	es,ax
+    mov	di,#0x0080
+    mov	cx,#0x10
+    rep
+    movsb
 
 ; Get hd1 data
 
-	mov	ax,#0x0000
-	mov	ds,ax
-	lds	si,[4*0x46]
-	mov	ax,#INITSEG
-	mov	es,ax
-	mov	di,#0x0090
-	mov	cx,#0x10
-	rep
-	movsb
+    mov	ax,#0x0000
+    mov	ds,ax
+    lds	si,[4*0x46]
+    mov	ax,#INITSEG
+    mov	es,ax
+    mov	di,#0x0090
+    mov	cx,#0x10
+    rep
+    movsb
 
 ; Check that there IS a hd1 :-)
 
-	mov	ax,#0x01500
-	mov	dl,#0x81
-	int	0x13
-	jc	no_disk1
-	cmp	ah,#3
-	je	is_disk1
+    mov	ax,#0x01500
+    mov	dl,#0x81
+    int	0x13
+    jc	no_disk1
+    cmp	ah,#3
+    je	is_disk1
 no_disk1:
-	mov	ax,#INITSEG
-	mov	es,ax
-	mov	di,#0x0090
-	mov	cx,#0x10
-	mov	ax,#0x00
-	rep
-	stosb
+    mov	ax,#INITSEG
+    mov	es,ax
+    mov	di,#0x0090
+    mov	cx,#0x10
+    mov	ax,#0x00
+    rep
+    stosb
 is_disk1:
 
 ; now we want to move to protected mode ...
 
-	cli			; no interrupts allowed ;
+    cli			; no interrupts allowed ;
 
 ; first we move the system to it's rightful place
 
-	mov	ax,#0x0000
-	cld			; 'direction'=0, movs moves forward
+    mov	ax,#0x0000
+    cld			; 'direction'=0, movs moves forward
 do_move:
-	mov	es,ax		; destination segment
-	add	ax,#0x1000
-	cmp	ax,#0x9000
-	jz	end_move
-	mov	ds,ax		; source segment
-	sub	di,di
-	sub	si,si
-	mov 	cx,#0x8000
-	rep
-	movsw
-	jmp	do_move
+    mov	es,ax		; destination segment
+    add	ax,#0x1000
+    cmp	ax,#0x9000
+    jz	end_move
+    mov	ds,ax		; source segment
+    sub	di,di
+    sub	si,si
+    mov 	cx,#0x8000
+    rep
+    movsw
+    jmp	do_move
 
 ; then we load the segment descriptors
 
 end_move:
-	mov	ax,#SETUPSEG	; right, forgot this at first. didn't work :-)
-	mov	ds,ax
-	lidt	idt_48		; load idt with 0,0
-	lgdt	gdt_48		; load gdt with whatever appropriate
+    mov	ax,#SETUPSEG	; right, forgot this at first. didn't work :-)
+    mov	ds,ax
+    lidt	idt_48		; load idt with 0,0
+    lgdt	gdt_48		; load gdt with whatever appropriate
 
 ; that was painless, now we enable A20
 
-	call	empty_8042
-	mov	al,#0xD1		; command write
-	out	#0x64,al
-	call	empty_8042
-	mov	al,#0xDF		; A20 on
-	out	#0x60,al
-	call	empty_8042
-
-; well, that went ok, I hope. Now we have to reprogram the interrupts :-(
-; we put them right after the intel-reserved hardware interrupts, at
-; int 0x20-0x2F. There they won't mess up anything. Sadly IBM really
-; messed this up with the original PC, and they haven't been able to
-; rectify it afterwards. Thus the bios puts interrupts at 0x08-0x0f,
-; which is used for the internal hardware interrupts as well. We just
-; have to reprogram the 8259's, and it isn't fun.
-
-	mov	al,#0x11		; initialization sequence
-	out	#0x20,al		; send it to 8259A-1
-	.word	0x00eb,0x00eb		; jmp $+2, jmp $+2
-	out	#0xA0,al		; and to 8259A-2
-	.word	0x00eb,0x00eb
-	mov	al,#0x20		; start of hardware int's (0x20)
-	out	#0x21,al
-	.word	0x00eb,0x00eb
-	mov	al,#0x28		; start of hardware int's 2 (0x28)
-	out	#0xA1,al
-	.word	0x00eb,0x00eb
-	mov	al,#0x04		; 8259-1 is master
-	out	#0x21,al
-	.word	0x00eb,0x00eb
-	mov	al,#0x02		; 8259-2 is slave
-	out	#0xA1,al
-	.word	0x00eb,0x00eb
-	mov	al,#0x01		; 8086 mode for both
-	out	#0x21,al
-	.word	0x00eb,0x00eb
-	out	#0xA1,al
-	.word	0x00eb,0x00eb
-	mov	al,#0xFF		; mask off all interrupts for now
-	out	#0x21,al
-	.word	0x00eb,0x00eb
-	out	#0xA1,al
+    call	empty_8042
+    mov	al,#0xD1		; command write
+    out	#0x64,al
+    call	empty_8042
+    mov	al,#0xDF		; A20 on
+    out	#0x60,al
+    call	empty_8042
+; 好吧，刚才的步骤还算顺利。现在我们必须重新编译中断:-(
+; 由于Intelx86架构保留了中断向量0x00-0x1F用于处理器内部异常（如除0错误、页面故障等）
+; 而IBM的工程师将可编程中断控制器(PIC)8259芯片的硬件中断(IRQ)映射到了中断向量0x08-0x0F
+; 这就导致了严重的冲突，例如中断0x08既是Inte1定义的双重错误异常，又是IRQ0(系统定时器中断)
+; 如果不退行重新映射，当硬件中断发生时，CPU可能会错误地执行异常处理程序，导致系统崩溃，
+; 由于IBMPC已成为事实标准，后续的PC兼容机都必须维持这个缺陷的设计以维持兼容性，
+; 我们需要重新编程8259芯片，将IRQ0-IRQ7映射到0x20-0x27(第一个8259芯片)
+; 将IRQ8-IRQ15映射到0x28-0x2F(第二个8259芯片)。
+; 0x20为8259A主片的命令端口，0x21为主8259A芯片的数据端口；0xA0为8259A从片的命令端口，0xA1为从8259A芯片的数据端口。
+    mov	al,#0x11		        ; 初始化命令字ICW，0x11=0b0001_0001
+    out	#0x20,al		        ; 发送到主8259A，0x20为主8259A命令端口。
+    .word	0x00eb,0x00eb		; 两个短跳转指令，作为延时
+    out	#0xA0,al		        ; 发送到从8259A，0xA0为从8259A命令端口
+    .word	0x00eb,0x00eb       ; 两个短跳转指令，作为延时
+    mov	al,#0x20		        ; 主片中断向量起始为0x20
+    out	#0x21,al                ; 0x21为主8259A数据端口。
+    .word	0x00eb,0x00eb
+    mov	al,#0x28		        ; 从片中断向量为0x28
+    out	#0xA1,al                ; 0xA1为从8259A数据端口。
+    .word	0x00eb,0x00eb
+    mov	al,#0x04		        ; 主片的IRQ2连接从片，0x04=0b0000_0100
+    out	#0x21,al
+    .word	0x00eb,0x00eb
+    mov	al,#0x02		        ; 0x02值表示从片连接到主片IRQ2
+    out	#0xA1,al
+    .word	0x00eb,0x00eb
+    mov	al,#0x01		        ; 8086模式(正常E0I)
+    out	#0x21,al
+    .word	0x00eb,0x00eb
+    out	#0xA1,al
+    .word	0x00eb,0x00eb
+    mov	al,#0xFF		        ; 屏蔽所有中断
+    out	#0x21,al
+    .word	0x00eb,0x00eb
+    out	#0xA1,al
 
 ; well, that certainly wasn't fun :-(. Hopefully it works, and we don't
 ; need no steenking BIOS anyway (except for the initial loading :-).
@@ -188,41 +188,41 @@ end_move:
 ; we let the gnu-compiled 32-bit programs do that. We just jump to
 ; absolute address 0x00000, in 32-bit protected mode.
 
-	mov	ax,#0x0001	; protected mode (PE) bit
-	lmsw	ax		; This is it;
-	jmpi	0,8		; jmp offset 0 of segment 8 (cs)
+    mov	ax,#0x0001	; protected mode (PE) bit
+    lmsw	ax		; This is it;
+    jmpi	0,8		; jmp offset 0 of segment 8 (cs)
 
 ; This routine checks that the keyboard command queue is empty
 ; No timeout is used - if this hangs there is something wrong with
 ; the machine, and we probably couldn't proceed anyway.
 empty_8042:
-	.word	0x00eb,0x00eb
-	in	al,#0x64	; 8042 status port
-	test	al,#2		; is input buffer full?
-	jnz	empty_8042	; yes - loop
-	ret
+    .word	0x00eb,0x00eb
+    in	al,#0x64	; 8042 status port
+    test	al,#2		; is input buffer full?
+    jnz	empty_8042	; yes - loop
+    ret
 
 gdt:
-	.word	0,0,0,0		; dummy
+    .word	0,0,0,0		; dummy
 
-	.word	0x07FF		; 8Mb - limit=2047 (2048*4096=8Mb)
-	.word	0x0000		; base address=0
-	.word	0x9A00		; code read/exec
-	.word	0x00C0		; granularity=4096, 386
+    .word	0x07FF		; 8Mb - limit=2047 (2048*4096=8Mb)
+    .word	0x0000		; base address=0
+    .word	0x9A00		; code read/exec
+    .word	0x00C0		; granularity=4096, 386
 
-	.word	0x07FF		; 8Mb - limit=2047 (2048*4096=8Mb)
-	.word	0x0000		; base address=0
-	.word	0x9200		; data read/write
-	.word	0x00C0		; granularity=4096, 386
+    .word	0x07FF		; 8Mb - limit=2047 (2048*4096=8Mb)
+    .word	0x0000		; base address=0
+    .word	0x9200		; data read/write
+    .word	0x00C0		; granularity=4096, 386
 
 idt_48:
-	.word	0			; idt limit=0
-	.word	0,0			; idt base=0L
+    .word	0			; idt limit=0
+    .word	0,0			; idt base=0L
 
 gdt_48:
-	.word	0x800		; gdt limit=2048, 256 GDT entries
-	.word	512+gdt,0x9	; gdt base = 0X9xxxx
-	
+    .word	0x800		; gdt limit=2048, 256 GDT entries
+    .word	512+gdt,0x9	; gdt base = 0X9xxxx
+    
 .text
 endtext:
 .data
