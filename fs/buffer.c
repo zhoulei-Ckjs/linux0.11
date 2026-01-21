c5bdf1cb4a6b06ffe522dfe4d61dffea0c570f55
zhoulei-Ckjs <56968749@qq.com>
2026-01-22 05:33:52 +0800

    20260122

--- fs/buffer.c
+++ fs/buffer.c
@@ -27,73 +27,73 @@
 #include <asm/io.h>
 
 extern int end;
-struct buffer_head * start_buffer = (struct buffer_head *) &end;		///< end 为代码段、数据段以及 bss 段的结束，定义在 link.ld 中
+struct buffer_head * start_buffer = (struct buffer_head *) &end;        ///< end 为代码段、数据段以及 bss 段的结束，定义在 link.ld 中
 struct buffer_head * hash_table[NR_HASH];
 static struct buffer_head * free_list;
 static struct task_struct * buffer_wait = NULL;
-int NR_BUFFERS = 0;														///< buffer 块个数
+int NR_BUFFERS = 0;         ///< buffer 块个数
 
 static inline void wait_on_buffer(struct buffer_head * bh)
 {
-	cli();																///< 关中断
-	while (bh->b_lock)
-		sleep_on(&bh->b_wait);
-	sti();																///< 开中断
+    cli();                  ///< 关中断
+    while (bh->b_lock)
+        sleep_on(&bh->b_wait);
+    sti();                  ///< 开中断
 }
 
 int sys_sync(void)
 {
-	int i;
-	struct buffer_head * bh;
+    int i;
+    struct buffer_head * bh;
 
-	sync_inodes();		/* write out inodes into buffers */
-	bh = start_buffer;
-	for (i=0 ; i<NR_BUFFERS ; i++,bh++) {
-		wait_on_buffer(bh);
-		if (bh->b_dirt)
-			ll_rw_block(WRITE,bh);
-	}
-	return 0;
+    sync_inodes();        /* write out inodes into buffers */
+    bh = start_buffer;
+    for (i=0 ; i<NR_BUFFERS ; i++,bh++) {
+        wait_on_buffer(bh);
+        if (bh->b_dirt)
+            ll_rw_block(WRITE,bh);
+    }
+    return 0;
 }
 
 int sync_dev(int dev)
 {
-	int i;
-	struct buffer_head * bh;
+    int i;
+    struct buffer_head * bh;
 
-	bh = start_buffer;
-	for (i=0 ; i<NR_BUFFERS ; i++,bh++) {
-		if (bh->b_dev != dev)
-			continue;
-		wait_on_buffer(bh);
-		if (bh->b_dev == dev && bh->b_dirt)
-			ll_rw_block(WRITE,bh);
-	}
-	sync_inodes();
-	bh = start_buffer;
-	for (i=0 ; i<NR_BUFFERS ; i++,bh++) {
-		if (bh->b_dev != dev)
-			continue;
-		wait_on_buffer(bh);
-		if (bh->b_dev == dev && bh->b_dirt)
-			ll_rw_block(WRITE,bh);
-	}
-	return 0;
+    bh = start_buffer;
+    for (i=0 ; i<NR_BUFFERS ; i++,bh++) {
+        if (bh->b_dev != dev)
+            continue;
+        wait_on_buffer(bh);
+        if (bh->b_dev == dev && bh->b_dirt)
+            ll_rw_block(WRITE,bh);
+    }
+    sync_inodes();
+    bh = start_buffer;
+    for (i=0 ; i<NR_BUFFERS ; i++,bh++) {
+        if (bh->b_dev != dev)
+            continue;
+        wait_on_buffer(bh);
+        if (bh->b_dev == dev && bh->b_dirt)
+            ll_rw_block(WRITE,bh);
+    }
+    return 0;
 }
 
 void inline invalidate_buffers(int dev)
 {
-	int i;
-	struct buffer_head * bh;
+    int i;
+    struct buffer_head * bh;
 
-	bh = start_buffer;
-	for (i=0 ; i<NR_BUFFERS ; i++,bh++) {
-		if (bh->b_dev != dev)
-			continue;
-		wait_on_buffer(bh);
-		if (bh->b_dev == dev)
-			bh->b_uptodate = bh->b_dirt = 0;
-	}
+    bh = start_buffer;
+    for (i=0 ; i<NR_BUFFERS ; i++,bh++) {
+        if (bh->b_dev != dev)
+            continue;
+        wait_on_buffer(bh);
+        if (bh->b_dev == dev)
+            bh->b_uptodate = bh->b_dirt = 0;
+    }
 }
 
 /*
@@ -112,17 +112,17 @@ void inline invalidate_buffers(int dev)
  */
 void check_disk_change(int dev)
 {
-	int i;
+    int i;
 
-	if (MAJOR(dev) != 2)
-		return;
-	if (!floppy_change(dev & 0x03))
-		return;
-	for (i=0 ; i<NR_SUPER ; i++)
-		if (super_block[i].s_dev == dev)
-			put_super(super_block[i].s_dev);
-	invalidate_inodes(dev);
-	invalidate_buffers(dev);
+    if (MAJOR(dev) != 2)
+        return;
+    if (!floppy_change(dev & 0x03))
+        return;
+    for (i=0 ; i<NR_SUPER ; i++)
+        if (super_block[i].s_dev == dev)
+            put_super(super_block[i].s_dev);
+    invalidate_inodes(dev);
+    invalidate_buffers(dev);
 }
 
 #define _hashfn(dev,block) (((unsigned)(dev^block))%NR_HASH)
@@ -131,46 +131,46 @@ void check_disk_change(int dev)
 static inline void remove_from_queues(struct buffer_head * bh)
 {
 /* remove from hash-queue */
-	if (bh->b_next)
-		bh->b_next->b_prev = bh->b_prev;
-	if (bh->b_prev)
-		bh->b_prev->b_next = bh->b_next;
-	if (hash(bh->b_dev,bh->b_blocknr) == bh)
-		hash(bh->b_dev,bh->b_blocknr) = bh->b_next;
+    if (bh->b_next)
+        bh->b_next->b_prev = bh->b_prev;
+    if (bh->b_prev)
+        bh->b_prev->b_next = bh->b_next;
+    if (hash(bh->b_dev,bh->b_blocknr) == bh)
+        hash(bh->b_dev,bh->b_blocknr) = bh->b_next;
 /* remove from free list */
-	if (!(bh->b_prev_free) || !(bh->b_next_free))
-		panic("Free block list corrupted");
-	bh->b_prev_free->b_next_free = bh->b_next_free;
-	bh->b_next_free->b_prev_free = bh->b_prev_free;
-	if (free_list == bh)
-		free_list = bh->b_next_free;
+    if (!(bh->b_prev_free) || !(bh->b_next_free))
+        panic("Free block list corrupted");
+    bh->b_prev_free->b_next_free = bh->b_next_free;
+    bh->b_next_free->b_prev_free = bh->b_prev_free;
+    if (free_list == bh)
+        free_list = bh->b_next_free;
 }
 
 static inline void insert_into_queues(struct buffer_head * bh)
 {
 /* put at end of free list */
-	bh->b_next_free = free_list;
-	bh->b_prev_free = free_list->b_prev_free;
-	free_list->b_prev_free->b_next_free = bh;
-	free_list->b_prev_free = bh;
+    bh->b_next_free = free_list;
+    bh->b_prev_free = free_list->b_prev_free;
+    free_list->b_prev_free->b_next_free = bh;
+    free_list->b_prev_free = bh;
 /* put the buffer in new hash-queue if it has a device */
-	bh->b_prev = NULL;
-	bh->b_next = NULL;
-	if (!bh->b_dev)									///< 0x300
-		return;
-	bh->b_next = hash(bh->b_dev,bh->b_blocknr);		///< 放入到 hash_table 中
-	hash(bh->b_dev,bh->b_blocknr) = bh;
-	bh->b_next->b_prev = bh;
+    bh->b_prev = NULL;
+    bh->b_next = NULL;
+    if (!bh->b_dev)            ///< 0x300
+        return;
+    bh->b_next = hash(bh->b_dev,bh->b_blocknr);        ///< 放入到 hash_table 中
+    hash(bh->b_dev,bh->b_blocknr) = bh;
+    bh->b_next->b_prev = bh;
 }
 
 static struct buffer_head * find_buffer(int dev, int block)
-{		
-	struct buffer_head * tmp;
+{        
+    struct buffer_head * tmp;
 
-	for (tmp = hash(dev,block) ; tmp != NULL ; tmp = tmp->b_next)
-		if (tmp->b_dev==dev && tmp->b_blocknr==block)
-			return tmp;
-	return NULL;
+    for (tmp = hash(dev,block) ; tmp != NULL ; tmp = tmp->b_next)
+        if (tmp->b_dev==dev && tmp->b_blocknr==block)
+            return tmp;
+    return NULL;
 }
 
 /*
@@ -182,21 +182,21 @@ static struct buffer_head * find_buffer(int dev, int block)
  */
 struct buffer_head * get_hash_table(int dev, int block)
 {
-	struct buffer_head * bh;
+    struct buffer_head * bh;
 
-	for (;;) {
-		if (!(bh=find_buffer(dev,block)))
-			return NULL;
-		bh->b_count++;
-		wait_on_buffer(bh);
-		if (bh->b_dev == dev && bh->b_blocknr == block)
-			return bh;
-		bh->b_count--;
-	}
+    for (;;) {
+        if (!(bh=find_buffer(dev,block)))
+            return NULL;
+        bh->b_count++;
+        wait_on_buffer(bh);
+        if (bh->b_dev == dev && bh->b_blocknr == block)
+            return bh;
+        bh->b_count--;
+    }
 }
 
 /*
- * Ok, this is getblk, and it isn't very clear, again to hinder				获取一个空闲链表头，如果当前块在 hash_table 里，则直接返回这个块
+ * Ok, this is getblk, and it isn't very clear, again to hinder                获取一个空闲链表头，如果当前块在 hash_table 里，则直接返回这个块
  * race-conditions. Most of the code is seldom used, (ie repeating),
  * so it should be much more efficient than it looks.
  *
@@ -205,87 +205,87 @@ struct buffer_head * get_hash_table(int dev, int block)
 #define BADNESS(bh) (((bh)->b_dirt<<1)+(bh)->b_lock)
 struct buffer_head * getblk(int dev,int block)
 {
-	struct buffer_head * tmp, * bh;
+    struct buffer_head * tmp, * bh;
 
 repeat:
-	if (bh = get_hash_table(dev,block))				///< 如果当前块在 hash_table 里，则直接返回
-		return bh;
-	tmp = free_list;								///< 可用内存循环链表头
-	do {
-		if (tmp->b_count)									///< 判断内存块是否被使用，是就继续找下一块
-			continue;
-		if (!bh || BADNESS(tmp)<BADNESS(bh)) {
-			bh = tmp;										///< 找到了内存块
-			if (!BADNESS(tmp))								///< 不是脏的 & 不被锁定
-				break;
-		}
+    if (bh = get_hash_table(dev,block))                     ///< 如果当前块在 hash_table 里，则直接返回
+        return bh;
+    tmp = free_list;                    ///< 可用内存循环链表头
+    do {
+        if (tmp->b_count)               ///< 判断内存块是否被使用，是就继续找下一块
+            continue;
+        if (!bh || BADNESS(tmp)<BADNESS(bh)) {
+            bh = tmp;                   ///< 找到了内存块
+            if (!BADNESS(tmp))          ///< 不是脏的 & 不被锁定
+                break;
+        }
 /* and repeat until we find something good */
-	} while ((tmp = tmp->b_next_free) != free_list);		///< 循环检查一圈，来寻找空闲内存块
-	if (!bh) {												///< 找了一圈都没找到，睡一觉重新找
-		sleep_on(&buffer_wait);
-		goto repeat;
-	}
-	wait_on_buffer(bh);
-	if (bh->b_count)
-		goto repeat;
-	while (bh->b_dirt) {
-		sync_dev(bh->b_dev);
-		wait_on_buffer(bh);
-		if (bh->b_count)
-			goto repeat;
-	}
+    } while ((tmp = tmp->b_next_free) != free_list);        ///< 循环检查一圈，来寻找空闲内存块
+    if (!bh) {                          ///< 找了一圈都没找到，睡一觉重新找
+        sleep_on(&buffer_wait);
+        goto repeat;
+    }
+    wait_on_buffer(bh);
+    if (bh->b_count)
+        goto repeat;
+    while (bh->b_dirt) {
+        sync_dev(bh->b_dev);
+        wait_on_buffer(bh);
+        if (bh->b_count)
+            goto repeat;
+    }
 /* NOTE!! While we slept waiting for this block, somebody else might */
 /* already have added "this" block to the cache. check it */
-	if (find_buffer(dev,block))
-		goto repeat;
+    if (find_buffer(dev,block))
+        goto repeat;
 /* OK, FINALLY we know that this buffer is the only one of it's kind, */
 /* and that it's unused (b_count=0), unlocked (b_lock=0), and clean */
-	bh->b_count=1;
-	bh->b_dirt=0;
-	bh->b_uptodate=0;						///< 并不是磁盘中最新的
-	remove_from_queues(bh);					///< 将内存块移出hash表和free_list
-	bh->b_dev=dev;
-	bh->b_blocknr=block;
-	insert_into_queues(bh);
-	return bh;
+    bh->b_count=1;
+    bh->b_dirt=0;
+    bh->b_uptodate=0;                           ///< 并不是磁盘中最新的
+    remove_from_queues(bh);                     ///< 将内存块移出hash表和free_list
+    bh->b_dev=dev;
+    bh->b_blocknr=block;
+    insert_into_queues(bh);
+    return bh;
 }
 
 void brelse(struct buffer_head * buf)
 {
-	if (!buf)
-		return;
-	wait_on_buffer(buf);
-	if (!(buf->b_count--))
-		panic("Trying to free free buffer");
-	wake_up(&buffer_wait);
+    if (!buf)
+        return;
+    wait_on_buffer(buf);
+    if (!(buf->b_count--))
+        panic("Trying to free free buffer");
+    wake_up(&buffer_wait);
 }
 
 /*
  * bread() reads a specified block and returns the buffer that contains
  * it. It returns NULL if the block was unreadable.
  */
-struct buffer_head * bread(int dev,int block)			///< dev = 0x300, block = 0
+struct buffer_head * bread(int dev,int block)               ///< dev = 0x300, block = 0
 {
-	struct buffer_head * bh;
+    struct buffer_head * bh;
 
-	if (!(bh=getblk(dev, block)))						///< 获取一个空闲链表头，这个链表头指向了空闲链表
-		panic("bread: getblk returned NULL\n");
-	if (bh->b_uptodate)
-		return bh;
-	ll_rw_block(READ, bh);
-	wait_on_buffer(bh);
-	if (bh->b_uptodate)
-		return bh;
-	brelse(bh);
-	return NULL;
+    if (!(bh=getblk(dev, block)))                           ///< 获取一个空闲链表头，这个链表头指向了空闲链表
+        panic("bread: getblk returned NULL\n");
+    if (bh->b_uptodate)                                     ///< 内存块最新（与硬盘内容一致），则返回内存块。如果不一致的话就需要等待读取硬盘块到内存块中。
+        return bh;
+    ll_rw_block(READ, bh);
+    wait_on_buffer(bh);
+    if (bh->b_uptodate)
+        return bh;
+    brelse(bh);
+    return NULL;
 }
 
 #define COPYBLK(from,to) \
 __asm__("cld\n\t" \
-	"rep\n\t" \
-	"movsl\n\t" \
-	::"c" (BLOCK_SIZE/4),"S" (from),"D" (to) \
-	:"cx","di","si")
+    "rep\n\t" \
+    "movsl\n\t" \
+    ::"c" (BLOCK_SIZE/4),"S" (from),"D" (to) \
+    :"cx","di","si")
 
 /*
  * bread_page reads four buffers into memory at the desired address. It's
@@ -295,23 +295,23 @@ __asm__("cld\n\t" \
  */
 void bread_page(unsigned long address,int dev,int b[4])
 {
-	struct buffer_head * bh[4];
-	int i;
+    struct buffer_head * bh[4];
+    int i;
 
-	for (i=0 ; i<4 ; i++)
-		if (b[i]) {
-			if (bh[i] = getblk(dev,b[i]))
-				if (!bh[i]->b_uptodate)
-					ll_rw_block(READ,bh[i]);
-		} else
-			bh[i] = NULL;
-	for (i=0 ; i<4 ; i++,address += BLOCK_SIZE)
-		if (bh[i]) {
-			wait_on_buffer(bh[i]);
-			if (bh[i]->b_uptodate)
-				COPYBLK((unsigned long) bh[i]->b_data,address);
-			brelse(bh[i]);
-		}
+    for (i=0 ; i<4 ; i++)
+        if (b[i]) {
+            if (bh[i] = getblk(dev,b[i]))
+                if (!bh[i]->b_uptodate)
+                    ll_rw_block(READ,bh[i]);
+        } else
+            bh[i] = NULL;
+    for (i=0 ; i<4 ; i++,address += BLOCK_SIZE)
+        if (bh[i]) {
+            wait_on_buffer(bh[i]);
+            if (bh[i]->b_uptodate)
+                COPYBLK((unsigned long) bh[i]->b_data,address);
+            brelse(bh[i]);
+        }
 }
 
 /*
@@ -321,61 +321,61 @@ void bread_page(unsigned long address,int dev,int b[4])
  */
 struct buffer_head * breada(int dev,int first, ...)
 {
-	va_list args;
-	struct buffer_head * bh, *tmp;
+    va_list args;
+    struct buffer_head * bh, *tmp;
 
-	va_start(args,first);
-	if (!(bh=getblk(dev,first)))
-		panic("bread: getblk returned NULL\n");
-	if (!bh->b_uptodate)
-		ll_rw_block(READ,bh);
-	while ((first=va_arg(args,int))>=0) {
-		tmp=getblk(dev,first);
-		if (tmp) {
-			if (!tmp->b_uptodate)
-				ll_rw_block(READA,bh);
-			tmp->b_count--;
-		}
-	}
-	va_end(args);
-	wait_on_buffer(bh);
-	if (bh->b_uptodate)
-		return bh;
-	brelse(bh);
-	return (NULL);
+    va_start(args,first);
+    if (!(bh=getblk(dev,first)))
+        panic("bread: getblk returned NULL\n");
+    if (!bh->b_uptodate)
+        ll_rw_block(READ,bh);
+    while ((first=va_arg(args,int))>=0) {
+        tmp=getblk(dev,first);
+        if (tmp) {
+            if (!tmp->b_uptodate)
+                ll_rw_block(READA,bh);
+            tmp->b_count--;
+        }
+    }
+    va_end(args);
+    wait_on_buffer(bh);
+    if (bh->b_uptodate)
+        return bh;
+    brelse(bh);
+    return (NULL);
 }
 
-void buffer_init(long buffer_end)				///< buffer_end = 4*1024*1024
+void buffer_init(long buffer_end)                   ///< buffer_end = 4*1024*1024
 {
-	struct buffer_head * h = start_buffer;		///< 代码段、数据段以及 bss 段的结束
-	void * b;
-	int i;
+    struct buffer_head * h = start_buffer;          ///< 代码段、数据段以及 bss 段的结束
+    void * b;
+    int i;
 
-	if (buffer_end == 1<<20)
-		b = (void *) (640*1024);
-	else
-		b = (void *) buffer_end;				///< b = 4M
-	while ( (b -= BLOCK_SIZE) >= ((void *) (h+1)) ) {		///< 从 4M 往前的每个 1K 都定义为 buffer_head，并进行初始化
-		h->b_dev = 0;
-		h->b_dirt = 0;
-		h->b_count = 0;
-		h->b_lock = 0;
-		h->b_uptodate = 0;
-		h->b_wait = NULL;
-		h->b_next = NULL;
-		h->b_prev = NULL;
-		h->b_data = (char *) b;					///< 把内存管理起来
-		h->b_prev_free = h-1;					///< 双向链表
-		h->b_next_free = h+1;
-		h++;
-		NR_BUFFERS++;
-		if (b == (void *) 0x100000)				///< 跳过 1M ~ 0xA0000 之间的内存（显存和bios ROM内存）
-			b = (void *) 0xA0000;
-	}
-	h--;										///< h++ 后还回去
-	free_list = start_buffer;
-	free_list->b_prev_free = h;					///< 双向循环链表
-	h->b_next_free = free_list;
-	for (i=0;i<NR_HASH;i++)						///< 307
-		hash_table[i]=NULL;						///< 初始化 hash_table
-}	
+    if (buffer_end == 1<<20)
+        b = (void *) (640*1024);
+    else
+        b = (void *) buffer_end;                    ///< b = 4M
+    while ( (b -= BLOCK_SIZE) >= ((void *) (h+1)) ) {        ///< 从 4M 往前的每个 1K 都定义为 buffer_head，并进行初始化
+        h->b_dev = 0;
+        h->b_dirt = 0;
+        h->b_count = 0;
+        h->b_lock = 0;
+        h->b_uptodate = 0;
+        h->b_wait = NULL;
+        h->b_next = NULL;
+        h->b_prev = NULL;
+        h->b_data = (char *) b;                     ///< 把内存管理起来
+        h->b_prev_free = h-1;                       ///< 双向链表
+        h->b_next_free = h+1;
+        h++;
+        NR_BUFFERS++;
+        if (b == (void *) 0x100000)                 ///< 跳过 1M ~ 0xA0000 之间的内存（显存和bios ROM内存）
+            b = (void *) 0xA0000;
+    }
+    h--;                ///< h++ 后还回去
+    free_list = start_buffer;
+    free_list->b_prev_free = h;                     ///< 双向循环链表
+    h->b_next_free = free_list;
+    for (i=0;i<NR_HASH;i++)                         ///< 307
+        hash_table[i]=NULL;                         ///< 初始化 hash_table
+}    
