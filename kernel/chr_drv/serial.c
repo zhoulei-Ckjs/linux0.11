82bb433075b757d39c71b1078fc6c8007cb3fb92
wg <123@123.com>
2026-08-12 17:25:56 +0800

    202608121

--- kernel/chr_drv/serial.c
+++ kernel/chr_drv/serial.c
@@ -17,15 +17,19 @@
 extern void rs1_interrupt(void);	///< 串口（RS-232 / UART）设备的中断处理函数 ，专用于处理 串行通信接口的接收（RX）和发送（TX）中断。
 extern void rs2_interrupt(void);	///< 串口2 设备的中断处理函数 ，专用于处理 串行通信接口的接收（RX）和发送（TX）中断。
 
+/**
+ * @brief 硬件初始化函数 ，用于将串口从默认状态配置为可通信的 2400 bps、8N1 格式、中断驱动 的串行接口。
+ * 完成此初始化后，串口即可响应中断事件（如数据到达），并配合 read()/write() 系统调用实现异步通信。
+ */
 static void init(int port)
 {
-	outb_p(0x80, port+3);	/* set DLAB of line control reg */
-	outb_p(0x30, port);		/* LS of divisor (48 -> 2400 bps */
-	outb_p(0x00, port+1);	/* MS of divisor */
-	outb_p(0x03, port+3);	/* reset DLAB */
-	outb_p(0x0b, port+4);	/* set DTR,RTS, OUT_2 */
+	outb_p(0x80, port+3);	/* 第7位DLAB设置为1，表示下一步操作为设置波特率。*/
+	outb_p(0x30, port);		/* 写分频系数低字节。设置波特率，2400 bps，设计到计算略。*/
+	outb_p(0x00, port+1);	/* 写入分频系数高字节（DLAB==1时为此功能）。MS of divisor */
+	outb_p(0x03, port+3);	/* 恢复DLAB设置。*/
+	outb_p(0x0b, port+4);	/* 设置调制解调控制寄存器。set DTR,RTS, OUT_2 */
 	outb_p(0x0d, port+1);	/* enable all intrs but writes */
-	(void)inb(port);		/* read data port to reset things (?) */
+	(void)inb(port);		/* 读取 RBR 清除可能的“幽灵中断”，确保初始化后状态干净。read data port to reset things (?) */
 }
 
 void rs_init(void)
